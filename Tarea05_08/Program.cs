34236099d334feba8def3d2438d11a1a07fc0bed
Carlos <Carlos@xtrene.com>
2021-06-25 18:36:26 +0200

    Agregar archivos de proyecto.

---
+++ Tarea05_08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tarea05_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int longitud = 10;
+            int[] vector = new int[longitud];
+
+            for (int i= 0; i< longitud; i++)
+            {
+                vector[i] = i;
+            }
+            Console.Write("Valores del vector de longitud {0}: ", longitud);
+            for (int i =0; i<longitud; i++)
+            {
+                Console.Write("{0}, ", vector[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
